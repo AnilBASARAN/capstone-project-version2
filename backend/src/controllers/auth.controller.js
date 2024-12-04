b30f09b1f5e8814e3908700eb9d786b56f74280c
AnilBASARAN <nlbsrn@gmail.com>
2024-12-05 02:05:18 +0300

    auth check handled

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -9,7 +9,7 @@ export const signup = async (req, res) => {
     if (!username || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
-
+// auth check handled
     if (password.length < 6) {
       return res.status(400).json({ message: "Password must be at least 6 characters" });
     }
